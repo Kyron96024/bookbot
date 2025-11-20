8cc2b0be212482eef582053c065c85dedc0c472e
Kyron96024 <gergoondo@gmail.com>
2025-11-20 16:47:33 +0100

    formated output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_each_char
+from stats import get_word_count, get_each_char, make_sorted_dict
 
 def get_book_text():
     with open("/home/gergo/workspace/github.com/BOOTDOTDEV/bookbot/bookbot/books/frankenstein.txt") as f:
@@ -8,10 +8,18 @@ def get_book_text():
 
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     text = get_book_text()
     num_words = get_word_count(text)
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     dict_chars = get_each_char(text)
-    print(dict_chars)
+    sorted_dic = make_sorted_dict(dict_chars)
+    for dic in sorted_dic:
+        if dic["char"].isalpha():
+            print(f"{dic["char"]}: {dic["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -6,10 +6,23 @@ def get_word_count(text):
     return counter
 
 def get_each_char(text):
+    list_dict = []
     char_dict = {}
     for char in text:
         if char.lower() in char_dict.keys():
             char_dict[char.lower()] += 1
         else:
             char_dict[char.lower()] = 1
-    return char_dict
+    for char in char_dict:
+        one_dict = {}
+        one_dict["char"] = char
+        one_dict["num"] = char_dict[char]
+        list_dict.append(one_dict)
+    return list_dict
+
+def sort_on(item):
+    return item["num"]
+
+def make_sorted_dict(dict_char_count):
+    dict_char_count.sort(reverse=True, key=sort_on)
+    return dict_char_count
